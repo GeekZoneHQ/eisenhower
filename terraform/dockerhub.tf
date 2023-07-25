76b2e6fc66c89a897f98ce091743dc2b055ab397
James Geddes <j@jamesgeddes.pro>
2023-07-26 00:46:58 +0100

    Create a dockerhub repo

---
+++ terraform/dockerhub.tf
@@ -0,0 +1,6 @@
+resource "dockerhub_repository" "main" {
+  name             = local.repo
+  namespace        = local.org
+  description      = "${local.repo} repository."
+  full_description = "See http://GitHub.com/GeekZoneHQ/${local.repo}"
+}
